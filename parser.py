5dd40feadb1cc62c5c7bce6a8ffd5b57b8b4a08e
dolandeer <dolandeering@gmail.com>
2026-03-10 16:12:46 -0400

    fixed, added parser prototype

---
+++ parser.py
@@ -0,0 +1,4 @@
+# helper functions to parse speciesnet output
+
+# getClasses(dict imageOutput) returns the classes identified alongside the confidence score
+# getPredictionScore(dict imageOutput) returns overall prediction score assigned by model
--- speciesnet_test.py
+++ speciesnet_test.py
@@ -2,10 +2,11 @@
 from speciesnet import SpeciesNet
 import os
 
+#constants
 model_name = 'kaggle:google/speciesnet/pyTorch/v4.0.2a/1' 
 model = SpeciesNet(model_name=model_name)
-
 image_path = "/Users/dolandeering/Pictures/squirrel.jpeg"
+
 
 if not os.path.exists(image_path):
     print(f"File not found: {image_path}")
@@ -16,11 +17,13 @@ else:
     
     # use run_mode='single_thread' to avoid multiprocessing (bad!!!!!)
     # python -c "from speciesnet import SpeciesNet; help(SpeciesNet.predict)" ||||| dumps function signature for all inputs
-    results = model.predict(
+    resultdict = model.predict(
         filepaths=[image_path], 
         run_mode='single_thread'
     )
+    print(type(resultdict)) # type <class 'dict'>
 
-    print(results) # RAW OUTPUT
-    data = results['predictions'] # PARSE JSON
-    print(data)
+    print(resultdict) # RAW OUTPUT
+
+    # FORMAT: Predictions is an array of predictions (multi image input), each index has an inner dict.
+    print(resultdict.get('predictions')[0].get('classifications').get('classes'))
