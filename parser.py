040c9c232a3fb34ce4946b1ff178f6487072efcf
dolandeer <dolandeering@gmail.com>
2026-03-13 14:24:05 -0400

    Wrote helper functions for understanding SpeciesNet output, integrated into speciesnet test function.

--- parser.py
+++ parser.py
@@ -1,4 +1,16 @@
 # helper functions to parse speciesnet output
 
-# getClasses(dict imageOutput) returns the classes identified alongside the confidence score
-# getPredictionScore(dict imageOutput) returns overall prediction score assigned by model
+def get_classes(result_dict: dict): # returns ALL identified animals, helper function not useful elsewhere
+    classes = result_dict.get('predictions')[0].get('classifications').get('classes')
+    output_classes = classes.copy()
+    for i in range(len(classes)):
+        output_classes[i] = classes[i].partition(';')[2]
+    return output_classes
+
+
+def get_highest_result(result_dict:dict): # return the highest identified thing, alongside its confidence
+    classes = result_dict.get('predictions')[0].get('classifications').get('classes')
+    scores = result_dict.get('predictions')[0].get('classifications').get('scores')
+    highest_output = classes.copy()[0].partition(';')[2]
+    output = (highest_output, scores[0]) # tuples are immutable, which is fine here
+    return output
