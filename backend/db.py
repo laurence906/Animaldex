a24f78a08c7568b1bc0ed89b17c8c66c04e76980
dolandeer <dolandeering@gmail.com>
2026-04-22 15:00:33 -0400

    got rid of extra stuff

--- backend/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-client = MongoClient(os.getenv("MONGO_URI"))
-db = client[os.getenv("DB_NAME")]
-users = db["users"]
